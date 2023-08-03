51ea92822f0a827990676fe4f5ca7d2891bc3343
Shah Haque <shahhaque@shahs-air.mynetworksettings.com>
2023-08-02 21:33:25 -0400

    adding for loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        //for loop
+        for (int i =6; i<=34; i= i+3){
+            System.out.println(i);
+        }
     }
+
 }
